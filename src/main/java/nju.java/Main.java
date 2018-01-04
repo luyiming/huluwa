f9589bdd59ed2708cb072821837c0ea7643dd34e
luyiming <luyimingchn@gmail.com>
2018-01-05 00:31:22 +0800

    Initial commit

---
+++ src/main/java/nju.java/Main.java
@@ -0,0 +1,13 @@
+package nju.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
